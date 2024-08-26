bf33a164bad6a98be44df8a8597dfa107560ef54
Idan Cohen <Idancohen1414@gmail.com>
2024-08-26 15:48:53 +0300

    Initial commit for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -451,7 +451,7 @@ db.connect(err => {
   console.log('Connected to the database');
 });
 
-  const PORT = 5000;
+const PORT = process.env.PORT || 5000;
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
